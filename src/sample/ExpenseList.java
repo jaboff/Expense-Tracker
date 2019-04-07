484341975438149f0db53b1a1e6defea22918669
Jimmy <jaboff@masonlive.gmu.edu>
2019-04-07 15:24:54 -0400

    AddExpense and RemoveExpesne

--- src/sample/ExpenseList.java
+++ src/sample/ExpenseList.java
@@ -1,8 +1,41 @@
 package sample;
 
 import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ExpenseList
 {
+    private static ArrayList<Expense> list = new ArrayList<Expense>();
+    private ArrayList<Expense> filteredList = new ArrayList<Expense>();
 
+    public int getSize(){
+        return list.size();
+    }
+
+    public boolean addExpense(Expense e){
+        if( list.add(e) )
+        {
+            return true;
+        }
+        else
+        {
+            list.ensureCapacity(2 * list.size() );
+            return this.addExpense(e);
+        }
+    }
+
+    public boolean removeExpense(Expense e){
+        return list.remove(e);
+    }
+
+    public void sortByDate()
+    {
+        Collections.sort(list, compareDate);
+    }
+
+    Comparator<Expense> compareDate = new Comparator<Expense>() {
+        private int compare(Expense e1, Expense e2){
+            return e1.getDate().compareTo(e2.getDate());
+        }
+    }
 }
